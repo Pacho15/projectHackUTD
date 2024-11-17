49ca90bf4f35b93b3073510696f5170439d2eaf1
chans100 <148670269+chans100@users.noreply.github.com>
2024-11-16 19:26:30 -0600

    working graph

---
+++ HackUTDToyota/components/ChartComponent.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LineChart } from 'react-native-chart-kit';
+import { Dimensions, View } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+
+const ChartComponent: React.FC = () => {
+  const data = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`,
+        strokeWidth: 2,
+      },
+    ],
+    legend: ['Revenue'],
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientTo: '#08130D',
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    style: { borderRadius: 16 },
+  };
+
+  return (
+    <View>
+      <LineChart
+        data={data}
+        width={screenWidth - 32}
+        height={220}
+        chartConfig={chartConfig}
+        bezier
+        style={{ borderRadius: 16 }}
+      />
+    </View>
+  );
+};
+
+export default ChartComponent;
